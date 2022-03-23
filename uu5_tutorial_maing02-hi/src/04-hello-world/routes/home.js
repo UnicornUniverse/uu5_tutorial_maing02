44d8e2a70c0ad68f8b21a29627ad314c32e1f9a6
Martin Farkaš <martin.farkas@unicorn.com>
2022-03-23 10:18:23 +0100

    Add lesson Hello world

---
+++ uu5_tutorial_maing02-hi/src/04-hello-world/routes/home.js
@@ -0,0 +1,50 @@
+//@@viewOn:imports
+import { createVisualComponent } from "uu5g05";
+import { withRoute } from "uu_plus4u5g02-app";
+
+import Config from "./config/config.js";
+//@@viewOff:imports
+
+//@@viewOn:constants
+//@@viewOff:constants
+
+//@@viewOn:css
+//@@viewOff:css
+
+//@@viewOn:helpers
+//@@viewOff:helpers
+
+let Home = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "Home",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {},
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {},
+  //@@viewOff:defaultProps
+
+  render() {
+    //@@viewOn:render
+    return (
+      <div>
+        <h1>My message to the World:</h1>
+        <div>
+          <b>Hello </b>
+          <i>World!</i>
+        </div>
+      </div>
+    );
+    //@@viewOff:render
+  },
+});
+
+Home = withRoute(Home, { authenticated: true });
+
+//@@viewOn:exports
+export { Home };
+export default Home;
+//@@viewOff:exports
