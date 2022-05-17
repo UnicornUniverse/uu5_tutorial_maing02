04a987eea7545b095a04ea83c58374bc0901f011
Martin Farkaš <martin.farkas@unicorn.com>
2022-05-17 10:11:22 +0200

    Add lesson Multilingualism

---
+++ uu5_tutorial_maing02-hi/src/15-multilingualism/bricks/joke/create-view.js
@@ -0,0 +1,95 @@
+//@@viewOn:imports
+import { createVisualComponent, PropTypes, Utils, useState } from "uu5g05";
+import { Button, useAlertBus } from "uu5g05-elements";
+import CreateForm from "./create-form.js";
+import Config from "./config/config.js";
+//@@viewOff:imports
+
+//@@viewOn:css
+const Css = {
+  button: () => Config.Css.css({ display: "block", margin: "0px auto" }),
+};
+//@@viewOff:css
+
+//@@viewOn:constants
+const Mode = {
+  BUTTON: "BUTTON",
+  FORM: "FORM",
+};
+//@@viewOff:constants
+
+//@@viewOn:helpers
+function CreateButton(props) {
+  return (
+    <Button {...props} colorScheme="primary" significance="highlighted" className={Css.button()}>
+      Create joke
+    </Button>
+  );
+}
+//@@viewOff:helpers
+
+const CreateView = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "CreateView",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {
+    onCreate: PropTypes.func,
+  },
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {
+    onCreate: () => {},
+  },
+  //@@viewOff:defaultProps
+
+  render(props) {
+    //@@viewOn:private
+    const [mode, setMode] = useState(Mode.BUTTON);
+    const { addAlert } = useAlertBus();
+
+    async function handleSubmit(event) {
+      let joke;
+
+      try {
+        joke = await props.jokeDataList.handlerMap.create(event.data.value);
+        props.jokeDataList.handlerMap.load();
+      } catch (error) {
+        console.error(error);
+        showError(error, "Joke creation failed!");
+      }
+
+      addAlert({
+        message: `Joke ${joke.name} has been created.`,
+        priority: "success",
+        durationMs: 2000,
+      });
+
+      setMode(Mode.BUTTON);
+    }
+    //@@viewOff:private
+
+    //@@viewOn:render
+    const attrs = Utils.VisualComponent.getAttrs(props);
+    let content;
+
+    switch (mode) {
+      case Mode.BUTTON:
+        content = <CreateButton onClick={() => setMode(Mode.FORM)} />;
+        break;
+      default:
+        content = <CreateForm onSubmit={handleSubmit} onCancel={() => setMode(Mode.BUTTON)} />;
+        break;
+    }
+
+    return <div {...attrs}>{content}</div>;
+    //@@viewOff:render
+  },
+});
+
+//@@viewOn:exports
+export { CreateView };
+export default CreateView;
+//@@viewOff:exports
---
+++ uu5_tutorial_maing02-hi/src/15-multilingualism/core/route-bar.js
@@ -0,0 +1,48 @@
+//@@viewOn:imports
+import { createVisualComponent, Lsi, useRoute } from "uu5g05";
+import { useSubAppData } from "uu_plus4u5g02";
+import Plus4U5App from "uu_plus4u5g02-app";
+
+import Config from "./config/config.js";
+import LSI from "../config/lsi.js";
+//@@viewOff:imports
+
+const RouteBar = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "RouteBar",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {},
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {},
+  //@@viewOff:defaultProps
+
+  render(props) {
+    //@@viewOn:private
+    const [, setRoute] = useRoute();
+    const subAppDataObject = useSubAppData();
+
+    const appActionList = [
+      { children: <Lsi lsi={LSI.menu.home} />, onClick: () => setRoute("home") },
+      { children: <Lsi lsi={LSI.menu.jokes} />, onClick: () => setRoute("jokes") },
+      { children: <Lsi lsi={LSI.menu.about} />, onClick: () => setRoute("about"), collapsed: true },
+    ];
+    //@@viewOff:private
+
+    //@@viewOn:render
+    return (
+      <Plus4U5App.RouteBar appActionList={appActionList} {...props}>
+        <Plus4U5App.RouteHeader title={"Lesson 15 - Multilingualism - " + subAppDataObject.data.name} />
+      </Plus4U5App.RouteBar>
+    );
+    //@@viewOff:render
+  },
+});
+
+//@@viewOn:exports
+export { RouteBar };
+export default RouteBar;
+//@@viewOff:exports
