4e9c7c5392690909420c2d3ff01c59527654cce3
Martin Farkaš <martin.farkas@unicorn.com>
2022-05-23 09:30:29 +0200

    Add lesson Modal

---
+++ uu5_tutorial_maing02-hi/src/16-modal/bricks/joke/list-view.js
@@ -0,0 +1,122 @@
+//@@viewOn:imports
+import { createVisualComponent, PropTypes, Utils, useRef, useLsi } from "uu5g05";
+import { Button, Pending, useAlertBus } from "uu5g05-elements";
+import importLsi from "../../lsi/import-lsi";
+import Tile from "./tile";
+import Config from "./config/config.js";
+//@@viewOff:imports
+
+//@@viewOn:css
+const Css = {
+  tile: () => Config.Css.css({ marginBottom: 24 }),
+  buttonArea: () => Config.Css.css({ textAlign: "center", marginBottom: 24 }),
+};
+//@@viewOff:css
+
+const ListView = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "ListView",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {
+    jokeDataList: PropTypes.object.isRequired,
+    identity: PropTypes.object.isRequired,
+    categoryList: PropTypes.array,
+    profileList: PropTypes.array,
+  },
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {
+    categoryList: [],
+    profileList: [],
+  },
+  //@@viewOff:defaultProps
+
+  render(props) {
+    //@@viewOn:private
+    const { addAlert } = useAlertBus();
+    const nextPageIndexRef = useRef(1);
+    const lsi = useLsi(importLsi, [ListView.uu5Tag]);
+
+    function showError(error, header = "") {
+      addAlert({
+        header,
+        message: error.message,
+        priority: "error",
+      });
+    }
+
+    async function handleDelete(jokeDataObject) {
+      try {
+        await jokeDataObject.handlerMap.delete();
+      } catch (error) {
+        console.error(error);
+        showError(error, lsi.deleteFail);
+        return;
+      }
+
+      addAlert({
+        message: Utils.String.format(lsi.deleteDone, jokeDataObject.data.name),
+        priority: "success",
+        durationMs: 2000,
+      });
+    }
+
+    async function handleUpdate(jokeDataObject) {
+      try {
+        await jokeDataObject.handlerMap.update();
+      } catch (error) {
+        console.error(error);
+        showError(error, lsi.updateFail);
+      }
+    }
+
+    async function handleLoadNext() {
+      try {
+        await props.jokeDataList.handlerMap.loadNext({ pageIndex: nextPageIndexRef.current, pageSize: 3 });
+        nextPageIndexRef.current++;
+      } catch (error) {
+        console.error(error);
+        showError(error, lsi.pageLoadFail);
+      }
+    }
+    //@@viewOff:private
+
+    //@@viewOn:render
+    const attrs = Utils.VisualComponent.getAttrs(props);
+    const jokeList = props.jokeDataList.data.filter((item) => item !== undefined);
+
+    return (
+      <div {...attrs}>
+        {jokeList.map((item) => (
+          <Tile
+            key={item.data.id}
+            jokeDataObject={item}
+            profileList={props.profileList}
+            identity={props.identity}
+            onDelete={handleDelete}
+            onUpdate={handleUpdate}
+            className={Css.tile()}
+            categoryList={props.categoryList}
+          />
+        ))}
+        <div className={Css.buttonArea()}>
+          {props.jokeDataList.state !== "pending" && (
+            <Button colorScheme="primary" onClick={handleLoadNext}>
+              {lsi.loadNext}
+            </Button>
+          )}
+          {props.jokeDataList.state === "pending" && <Pending />}
+        </div>
+      </div>
+    );
+    //@@viewOff:render
+  },
+});
+
+//@@viewOn:exports
+export { ListView };
+export default ListView;
+//@@viewOff:exports
---
+++ uu5_tutorial_maing02-hi/src/16-modal/bricks/joke/tile.js
@@ -0,0 +1,208 @@
+//@@viewOn:imports
+import { createVisualComponent, PropTypes, Utils, useEffect, useLsi } from "uu5g05";
+import { Box, Text, Line, Button, DateTime, Pending } from "uu5g05-elements";
+import importLsi from "../../lsi/import-lsi.js";
+import Config from "./config/config.js";
+//@@viewOff:imports
+
+//@@viewOn:css
+const Css = {
+  main: () =>
+    Config.Css.css({
+      display: "flex",
+      flexDirection: "column",
+      height: "100%",
+    }),
+
+  header: () =>
+    Config.Css.css({
+      display: "block",
+      padding: 16,
+      height: 48,
+    }),
+
+  content: (image) =>
+    Config.Css.css({
+      display: "flex",
+      alignItems: image ? "center" : "left",
+      justifyContent: image ? "center" : "flex-start",
+      height: "calc(100% - 48px - 48px)",
+      overflow: "hidden",
+    }),
+
+  text: () =>
+    Config.Css.css({
+      display: "block",
+      marginLeft: 16,
+      marginRight: 16,
+      marginBottom: 16,
+    }),
+
+  image: () => Config.Css.css({ width: "100%" }),
+
+  footer: () =>
+    Config.Css.css({
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      height: 48,
+      marginTop: 8,
+      paddingLeft: 16,
+      paddingRight: 8,
+    }),
+
+  infoLine: () =>
+    Config.Css.css({
+      display: "block",
+      marginLeft: 16,
+      marginTop: 8,
+    }),
+};
+//@@viewOff:css
+
+//@@viewOn:helpers
+function InfoLine({ children }) {
+  return (
+    <Text
+      category="interface"
+      segment="content"
+      type="medium"
+      significance="subdued"
+      colorScheme="building"
+      className={Css.infoLine()}
+    >
+      {children}
+    </Text>
+  );
+}
+
+function hasManagePermission(joke, identity, profileList) {
+  const isAuthority = profileList.includes("Authorities");
+  const isExecutive = profileList.includes("Executives");
+  const isOwner = joke.uuIdentity === identity.uuIdentity;
+  return isAuthority || (isExecutive && isOwner);
+}
+//@@viewOff:helpers
+
+const Tile = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "Tile",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {
+    jokeDataObject: PropTypes.object.isRequired,
+    categoryList: PropTypes.array,
+    onUpdate: PropTypes.func,
+    onDelete: PropTypes.func,
+  },
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {
+    categoryList: [],
+    onUpdate: () => {},
+    onDelete: () => {},
+  },
+  //@@viewOff:defaultProps
+
+  render(props) {
+    //@@viewOn:private
+    const lsi = useLsi(importLsi, [Tile.uu5Tag]);
+
+    useEffect(() => {
+      if (
+        props.jokeDataObject.data.image &&
+        !props.jokeDataObject.data.imageUrl &&
+        props.jokeDataObject.state === "ready" &&
+        props.jokeDataObject.handlerMap?.getImage
+      ) {
+        props.jokeDataObject.handlerMap.getImage(props.jokeDataObject.data).catch((error) => console.error(error));
+      }
+    }, [props.jokeDataObject]);
+
+    function handleDelete() {
+      props.onDelete(props.jokeDataObject);
+    }
+
+    function handleUpdate() {
+      props.onUpdate(props.jokeDataObject);
+    }
+
+    function buildCategoryNames(categoryIdList) {
+      // for faster lookup
+      let categoryIds = new Set(categoryIdList);
+      return props.categoryList
+        .reduce((acc, category) => {
+          if (categoryIds.has(category.id)) {
+            acc.push(category.name);
+          }
+          return acc;
+        }, [])
+        .join(", ");
+    }
+    //@@viewOff:private
+
+    //@@viewOn:render
+    const [elementProps] = Utils.VisualComponent.splitProps(props, Css.main());
+    const joke = props.jokeDataObject.data;
+    const canManage = hasManagePermission(joke, props.identity, props.profileList);
+    const isActionDisabled = props.jokeDataObject.state === "pending";
+
+    return (
+      <Box {...elementProps}>
+        <Text category="interface" segment="title" type="minor" colorScheme="building" className={Css.header()}>
+          {joke.name}
+        </Text>
+
+        <div className={Css.content(joke.image)}>
+          {joke.text && !joke.image && (
+            <Text category="interface" segment="content" type="medium" colorScheme="building" className={Css.text()}>
+              {joke.text}
+            </Text>
+          )}
+          {joke.imageUrl && <img src={joke.imageUrl} alt={joke.name} className={Css.image()} />}
+          {joke.image && !joke.imageUrl && <Pending size="xl" />}
+        </div>
+
+        <Line significance="subdued" />
+
+        {joke.categoryIdList?.length > 0 && <InfoLine>{buildCategoryNames(joke.categoryIdList)}</InfoLine>}
+
+        <InfoLine>{joke.uuIdentityName}</InfoLine>
+
+        <InfoLine>
+          <DateTime value={joke.sys.cts} dateFormat="short" timeFormat="none" />
+        </InfoLine>
+
+        <Box significance="distinct" className={Css.footer()}>
+          {Utils.String.format(lsi.averageRating, joke.averageRating)}
+          {canManage && (
+            <div>
+              <Button
+                icon="mdi-pencil"
+                onClick={handleUpdate}
+                significance="subdued"
+                tooltip={lsi.updateTip}
+                disabled={isActionDisabled}
+              />
+              <Button
+                icon="mdi-delete"
+                onClick={handleDelete}
+                significance="subdued"
+                tooltip={lsi.deleteTip}
+                disabled={isActionDisabled}
+              />
+            </div>
+          )}
+        </Box>
+      </Box>
+    );
+    //@@viewOff:render
+  },
+});
+
+//@@viewOn:exports
+export { Tile };
+export default Tile;
+//@@viewOff:exports
