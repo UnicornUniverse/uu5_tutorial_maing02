29c167dcb29671731e8c03324e3ef8c38e98c578
Martin Farkaš <martin.farkas@unicorn.com>
2022-05-25 16:41:14 +0200

    Create lesson Forms

---
+++ uu5_tutorial_maing02-hi/src/17-forms/bricks/joke/list-view.js
@@ -0,0 +1,157 @@
+//@@viewOn:imports
+import { createVisualComponent, PropTypes, Utils, useRef, useLsi, useState } from "uu5g05";
+import { Button, Pending, useAlertBus } from "uu5g05-elements";
+import importLsi from "../../lsi/import-lsi";
+import Tile from "./tile";
+import Config from "./config/config.js";
+import DetailModal from "./detail-modal";
+//@@viewOff:imports
+
+//@@viewOn:css
+const Css = {
+  tile: () => Config.Css.css({ marginBottom: 24 }),
+  buttonArea: () => Config.Css.css({ textAlign: "center", marginBottom: 24 }),
+};
+//@@viewOff:css
+
+//@@viewOn:helpers
+function getJokeDataObject(jokeDataList, id) {
+  // HINT: We need to also check newData where are newly created items
+  // that don't meet filtering, sorting or paging criteria.
+  const item =
+    jokeDataList.newData?.find((item) => item?.data.id === id) ||
+    jokeDataList.data.find((item) => item?.data.id === id);
+
+  return item;
+}
+//@@viewOff:helpers
+
+const ListView = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "ListView",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {
+    jokeDataList: PropTypes.object.isRequired,
+    identity: PropTypes.object.isRequired,
+    categoryList: PropTypes.array,
+    profileList: PropTypes.array,
+  },
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {
+    categoryList: [],
+    profileList: [],
+  },
+  //@@viewOff:defaultProps
+
+  render(props) {
+    //@@viewOn:private
+    const { addAlert } = useAlertBus();
+    const nextPageIndexRef = useRef(1);
+    const lsi = useLsi(importLsi, [ListView.uu5Tag]);
+    const [itemDetailData, setItemDetailData] = useState({ open: false, id: undefined });
+    let activeDataObject;
+
+    if (itemDetailData.id) {
+      activeDataObject = getJokeDataObject(props.jokeDataList, itemDetailData.id);
+    }
+
+    function showError(error, header = "") {
+      addAlert({
+        header,
+        message: error.message,
+        priority: "error",
+      });
+    }
+
+    async function handleDelete(jokeDataObject) {
+      try {
+        await jokeDataObject.handlerMap.delete();
+      } catch (error) {
+        console.error(error);
+        showError(error, lsi.deleteFail);
+        return;
+      }
+
+      addAlert({
+        message: Utils.String.format(lsi.deleteDone, jokeDataObject.data.name),
+        priority: "success",
+        durationMs: 2000,
+      });
+    }
+
+    async function handleUpdate(jokeDataObject) {
+      try {
+        await jokeDataObject.handlerMap.update();
+      } catch (error) {
+        console.error(error);
+        showError(error, lsi.updateFail);
+      }
+    }
+
+    async function handleLoadNext() {
+      try {
+        await props.jokeDataList.handlerMap.loadNext({ pageIndex: nextPageIndexRef.current, pageSize: 3 });
+        nextPageIndexRef.current++;
+      } catch (error) {
+        console.error(error);
+        showError(error, lsi.pageLoadFail);
+      }
+    }
+
+    const handleItemDetailOpen = (jokeDataObject) => setItemDetailData({ open: true, id: jokeDataObject.data.id });
+    const handleItemDetailClose = () => setItemDetailData({ open: false });
+    //@@viewOff:private
+
+    //@@viewOn:render
+    const attrs = Utils.VisualComponent.getAttrs(props);
+    const jokeList = props.jokeDataList.data.filter((item) => item !== undefined);
+
+    return (
+      <div {...attrs}>
+        {jokeList.map((item) => (
+          <Tile
+            key={item.data.id}
+            jokeDataObject={item}
+            profileList={props.profileList}
+            identity={props.identity}
+            onDetail={handleItemDetailOpen}
+            onDelete={handleDelete}
+            onUpdate={handleUpdate}
+            className={Css.tile()}
+            categoryList={props.categoryList}
+          />
+        ))}
+        <div className={Css.buttonArea()}>
+          {props.jokeDataList.state !== "pending" && (
+            <Button colorScheme="primary" onClick={handleLoadNext}>
+              {lsi.loadNext}
+            </Button>
+          )}
+          {props.jokeDataList.state === "pending" && <Pending />}
+        </div>
+        {itemDetailData.open && activeDataObject && (
+          <DetailModal
+            jokeDataObject={activeDataObject}
+            profileList={props.profileList}
+            identity={props.identity}
+            categoryList={props.categoryList}
+            onDelete={handleDelete}
+            onUpdate={handleUpdate}
+            onClose={handleItemDetailClose}
+            open
+          />
+        )}
+      </div>
+    );
+    //@@viewOff:render
+  },
+});
+
+//@@viewOn:exports
+export { ListView };
+export default ListView;
+//@@viewOff:exports
