46508b659643f749dd61c0596f48058e0265eeb1
Martin Farkaš <martin.farkas@unicorn.com>
2022-05-30 19:33:09 +0200

    Rename lessons

---
+++ uu5_tutorial_maing02-hi/src/16-tiles/bricks/joke/list-provider.js
@@ -0,0 +1,88 @@
+//@@viewOn:imports
+import { createComponent, useDataList, useEffect, useRef } from "uu5g05";
+import Config from "./config/config";
+import Calls from "calls";
+//@@viewOff:imports
+
+export const ListProvider = createComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "ListProvider",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {},
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {},
+  //@@viewOff:defaultProps
+
+  render(props) {
+    //@@viewOn:private
+    const jokeDataList = useDataList({
+      handlerMap: {
+        load: handleLoad,
+        loadNext: handleLoadNext,
+        create: handleCreate,
+      },
+      itemHandlerMap: {
+        update: handleUpdate,
+        delete: handleDelete,
+        getImage: handleGetImage,
+      },
+    });
+
+    const imageUrlListRef = useRef([]);
+
+    function handleLoad() {
+      const dtoIn = { pageInfo: { pageSize: 50 } };
+      return Calls.Joke.list(dtoIn);
+    }
+
+    function handleLoadNext(pageInfo) {
+      const dtoIn = { pageInfo };
+      return Calls.Joke.list(dtoIn);
+    }
+
+    function handleCreate(values) {
+      return Calls.Joke.create(values);
+    }
+
+    function handleUpdate(values) {
+      return Calls.Joke.update(values);
+    }
+
+    function handleDelete(joke) {
+      const dtoIn = { id: joke.id };
+      return Calls.Joke.delete(dtoIn, props.baseUri);
+    }
+
+    async function handleGetImage(joke) {
+      const dtoIn = { code: joke.image };
+      const imageFile = await Calls.Joke.getImage(dtoIn);
+      const imageUrl = generateAndRegisterImageUrl(imageFile);
+      return { ...joke, imageFile, imageUrl };
+    }
+
+    function generateAndRegisterImageUrl(imageFile) {
+      const imageUrl = URL.createObjectURL(imageFile);
+      imageUrlListRef.current.push(imageUrl);
+      return imageUrl;
+    }
+
+    useEffect(() => {
+      // We don't use it to store reference on another React component
+      // eslint-disable-next-line uu5/hooks-exhaustive-deps
+      return () => imageUrlListRef.current.forEach((url) => URL.revokeObjectURL(url));
+      // We want to trigger this effect only once.
+      // eslint-disable-next-line uu5/hooks-exhaustive-deps
+    }, []);
+    //@@viewOff:private
+
+    //@@viewOn:render
+    return typeof props.children === "function" ? props.children(jokeDataList) : props.children;
+    //@@viewOff:render
+  },
+});
+
+export default ListProvider;
---
+++ uu5_tutorial_maing02-hi/src/16-tiles/bricks/joke/list-view.js
@@ -0,0 +1,180 @@
+//@@viewOn:imports
+import { createVisualComponent, PropTypes, Utils, useRef, useLsi, useState } from "uu5g05";
+import { useAlertBus } from "uu5g05-elements";
+import { Grid } from "uu5tilesg02";
+import importLsi from "../../lsi/import-lsi";
+import Tile from "./tile";
+import Config from "./config/config.js";
+import DetailModal from "./detail-modal";
+import UpdateModal from "./update-modal";
+//@@viewOff:imports
+
+//@@viewOn:css
+//@@viewOff:css
+
+//@@viewOn:helpers
+function getJokeDataObject(jokeDataList, id) {
+  // HINT: We need to also check newData where are newly created items
+  // that don't meet filtering, sorting or paging criteria.
+  const item =
+    jokeDataList.newData?.find((item) => item?.data.id === id) ||
+    jokeDataList.data.find((item) => item?.data.id === id);
+
+  return item;
+}
+//@@viewOff:helpers
+
+const ListView = createVisualComponent({
+  //@@viewOn:statics
+  uu5Tag: Config.TAG + "ListView",
+  //@@viewOff:statics
+
+  //@@viewOn:propTypes
+  propTypes: {
+    jokeDataList: PropTypes.object.isRequired,
+    identity: PropTypes.object.isRequired,
+    categoryList: PropTypes.array,
+    profileList: PropTypes.array,
+  },
+  //@@viewOff:propTypes
+
+  //@@viewOn:defaultProps
+  defaultProps: {
+    categoryList: [],
+    profileList: [],
+  },
+  //@@viewOff:defaultProps
+
+  render(props) {
+    //@@viewOn:private
+    const { addAlert } = useAlertBus();
+    const nextPageIndexRef = useRef(1);
+    const lsi = useLsi(importLsi, [ListView.uu5Tag]);
+    const [detailData, setDetailData] = useState({ open: false, id: undefined });
+    const [updateData, setUpdateData] = useState({ open: false, id: undefined });
+
+    const activeDataObjectId = detailData.id || updateData.id;
+    let activeDataObject;
+
+    if (activeDataObjectId) {
+      activeDataObject = getJokeDataObject(props.jokeDataList, activeDataObjectId);
+    }
+
+    function showError(error, header = "") {
+      addAlert({
+        header,
+        message: error.message,
+        priority: "error",
+      });
+    }
+
+    async function handleDelete(jokeDataObject) {
+      try {
+        await jokeDataObject.handlerMap.delete();
+      } catch (error) {
+        console.error(error);
+        showError(error, lsi.deleteFail);
+        return;
+      }
+
+      addAlert({
+        message: Utils.String.format(lsi.deleteDone, jokeDataObject.data.name),
+        priority: "success",
+        durationMs: 2000,
+      });
+    }
+
+    async function handleUpdate(jokeDataObject) {
+      setUpdateData({ open: true, id: jokeDataObject.data.id });
+    }
+
+    async function handleUpdateSubmit(jokeDataObject, values) {
+      try {
+        await jokeDataObject.handlerMap.update(values);
+      } catch (error) {
+        console.error(error);
+        showError(error, lsi.updateFail, error);
+        return;
+      }
+
+      setUpdateData({ open: false });
+    }
+
+    function handleUpdateCancel() {
+      setUpdateData({ open: false });
+    }
+
+    async function handleLoadNext({ indexFrom }) {
+      const pageSize = props.jokeDataList.pageSize;
+
+      try {
+        await props.jokeDataList.handlerMap.loadNext({
+          pageSize: pageSize,
+          pageIndex: Math.floor(indexFrom / pageSize),
+        });
+        nextPageIndexRef.current++;
+      } catch (error) {
+        console.error(error);
+        showError(error, lsi.pageLoadFail);
+      }
+    }
+
+    const handleItemDetailOpen = (jokeDataObject) => setDetailData({ open: true, id: jokeDataObject.data.id });
+    const handleItemDetailClose = () => setDetailData({ open: false });
+    //@@viewOff:private
+
+    //@@viewOn:render
+    const attrs = Utils.VisualComponent.getAttrs(props);
+
+    const tileProps = {
+      profileList: props.profileList,
+      identity: props.identity,
+      categoryList: props.categoryList,
+      onDetail: handleItemDetailOpen,
+      onDelete: handleDelete,
+      onUpdate: handleUpdate,
+    };
+
+    return (
+      <div {...attrs}>
+        <Grid
+          data={props.jokeDataList.data}
+          onLoad={handleLoadNext}
+          rowSpacing={8}
+          tileHeight={300}
+          emptyStateLabel={lsi.noJokes}
+          virtualization
+        >
+          <Tile {...tileProps} />
+        </Grid>
+        {detailData.open && activeDataObject && (
+          <DetailModal
+            jokeDataObject={activeDataObject}
+            profileList={props.profileList}
+            identity={props.identity}
+            categoryList={props.categoryList}
+            onDelete={handleDelete}
+            onUpdate={handleUpdate}
+            onClose={handleItemDetailClose}
+            open
+          />
+        )}
+        {updateData.open && (
+          <UpdateModal
+            jokeDataObject={activeDataObject}
+            categoryList={props.categoryList}
+            onSubmit={handleUpdateSubmit}
+            onCancel={handleUpdateCancel}
+            open
+          />
+        )}
+      </div>
+    );
+    //@@viewOff:render
+  },
+});
+
+//@@viewOn:exports
+export { ListView };
+export default ListView;
+//@@viewOff:exports
